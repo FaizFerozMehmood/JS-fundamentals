e76c90463714265504695f931c9adcf9300faba0
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-10 13:17:53 +0500

    initial

---
+++ string/app.js
@@ -0,0 +1,9 @@
+
+function capitalize(word) {
+    return word.slice(0,1).toUpperCase()+word.slice(1).toLowerCase()
+
+}
+
+console.log(capitalize("sam")); 
+console.log(capitalize("ALEX"));
+console.log(capitalize("chARLie"));
