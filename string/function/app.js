b202488989cef24aa3a6bfc7f9ef96c0923e0747
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-11 08:48:08 +0500

    commit

--- string/function/app.js
+++ string/function/app.js
@@ -1,6 +1,28 @@
+//  default parameters in function
  function buyProducts(name1 = "milk",name2 = "aalo",name3 = "channy k daal"){
     console.log(`remember to buy ${name1}`);
     console.log(`remember to buy ${name2}`);
     console.log(`remember to buy ${name3}`);
  }
 buyProducts()
+
+//++++++++++++++RETURN++++
+function sayHello(firstName,lastName){
+    return firstName +" "+ lastName
+
+}
+ const fulName = sayHello("faiz","ali")
+ console.log(fulName);
+
+        // ++++++++++++++++++
+        
+ function multiplyByNineFifths(number) {
+    return number * (9/5);
+  };
+  
+  function getFahrenheit(celsius) {
+    return multiplyByNineFifths(celsius) + 32;
+  };
+  
+  getFahrenheit(15);
+ 
