9345406b44df71242e264f9a4480ee1f6a4003d7
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-17 13:07:36 +0500

    commit

--- conditional statements/app.js
+++ conditional statements/app.js
@@ -2,15 +2,15 @@ const textInput = document.getElementById("textInput")
 
 textInput.addEventListener('change',()=>{
     // console.log(textInput.value);
-    // textInput.value=""
     let vl=textInput.value
     if(vl.length > 10){
-       console.log(vl.substring(0,10)+"...") 
-      
+        console.log(vl.substring(0,10)+"...") 
+        
     }
     else{
-    console.log("length is less than 10");
-    
+        console.log("length is less than 10");
+        
     }
+    textInput.value=""
 })
 // ++++++++++++++++++     substring(1,4) eg :(hello)   => ell, takes two parameter,starting and ending excluding and one perameter  eg:2 to end     ++++++++++++++++++++
