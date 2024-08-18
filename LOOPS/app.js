47378ac57d4b2cb22acabd67c2d2d1bf49436de0
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-18 20:28:45 +0500

    commit

---
+++ LOOPS/app.js
@@ -0,0 +1,7 @@
+ function useLoop(ele){
+ ele.forEach(function(ele){
+    console.log(ele);
+    
+ })
+ }
+ useLoop([1,2,3,4,5,6])
---
+++ Math expressions/app.js
@@ -0,0 +1,6 @@
+var num = 2;
+var newNum = num--
+console.log(newNum);
+const prmt = prompt('enter')
+console.log(prmt);
+
