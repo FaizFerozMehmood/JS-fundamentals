922d6aa3927c70f3d90360ee74025bea9bce7db1
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-18 13:06:12 +0500

    commit

--- ARRAY/app.js
+++ ARRAY/app.js
@@ -1,3 +1,17 @@
+//push() adds an element at the end of an array and returns new length
 const arr = [1,2,3,4,5]
 const vlu = arr.push("clock")
 console.log(vlu);
+
+
+
+//returning first element from an array
+const arry = [1,2,3,45,]
+const arry2 = arry[0]
+console.log(arry2);
+
+// returning last element of the array using at()
+
+const ary = [1,2,3,44,5,6,7]
+const vl = ary.at(-1)
+console.log(vl);
