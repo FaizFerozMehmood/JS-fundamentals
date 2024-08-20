b498de23ac6066f31a8e5b0e396777110b776841
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-20 12:19:17 +0500

    commit

--- LOOPS/app.js
+++ LOOPS/app.js
@@ -5,3 +5,23 @@
  })
  }
  useLoop([1,2,3,4,5,6])
+
+
+
+//  Sum grades 
+
+// Complete the function sumGrades such that it
+//  returns the sum of all the grades it receives as
+//   a parameter. We haven't seen reduce yet, so try to
+//    solve it using what you have learned so far.
+
+function sumGrades(grades){
+   let sum = 0;
+   grades.forEach(function(grade){
+      sum+=grade
+
+   })
+   return sum
+
+}
+sumGrades([1,2,3,4,6])
