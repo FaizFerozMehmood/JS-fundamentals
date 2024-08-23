5545fbd58f1c8da98dd836099af656ed871c1f58
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-23 18:33:56 +0500

    commit

--- Filter/app.js
+++ Filter/app.js
@@ -1,4 +1,19 @@
 const arr = [12,324,45,6,5767,8]
 const even = arr.filter((data,index)=>data %2 ===0)
-const odd = arr.filter((data,index )=> data<10)
+const odd = arr.filter((data,index )=>{
+    return data<10
+    
+})
 console.log(odd);
+let zero = []
+
+const vlue = [1,0,6,0,0]
+vlue.forEach((data,index)=>{
+    // console.log(data);
+    if(data <1){
+        vlue.push(zero)
+        console.log(data);
+        
+    }
+    
+})
