44028205a8250b3369755ab233e54c9c1c2661e2
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-09-22 11:16:55 +0500

    commit

--- JS basic questions/app.js
+++ JS basic questions/app.js
@@ -77,32 +77,22 @@ function myfun(e) {
 }
 // myfun("abchdhdhdh g hdg");
 
-
-let oddNum = ""
-let evenNum = ""
-for(var i = 0; i < 10; i++){
-if(i % 2 ===0){
-  let result = evenNum + i;
-  // console.log(`Even number  :${result}`);
-  
-  
-}  
-else{
-  let oddres = oddNum + i
-  // console.log(`Odd number  :${oddres}`);
-  
-}
+let oddNum = "";
+let evenNum = "";
+for (var i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    let result = evenNum + i;
+    // console.log(`Even number  :${result}`);
+  } else {
+    let oddres = oddNum + i;
+    // console.log(`Odd number  :${oddres}`);
+  }
 }
 
-function myFun(e){
-
+function myFun(e) {
   //  console.log(e.toString().includes("k"));
-   
-   
 }
 // myFun(["faiz","mahmood"])
-
-
 
 // function isContains(valu){
 
@@ -110,8 +100,6 @@ function myFun(e){
 //      let myNum =["9"]
 //      let findOut = valu.includes("4")
 //      console.log(findOut);
-     
-     
 
 // }
 // isContains(["123","4","56","7"])
@@ -123,7 +111,6 @@ function myFun(e){
 // })
 // let result ="#"+ res.join("")
 // return result
-
 
 // }
 // console.log(myfUnction("Hello i am faiz"))
@@ -137,7 +124,6 @@ function myFun(e){
 // console.log(getResult(3,4,3));
 // console.log(getResult(6,9,7));
 
-
 // function fn(e){
 //   // console.log(e.split());
 //   let convert = e.split()
@@ -145,14 +131,10 @@ function myFun(e){
 //   return word[0].toUpperCase()+word.slice(1).toLowerCaseCase()
 //   console.log(res);
 //   })
-  
-  
+
 // }
 
-
 // console.log(fn("hello javascript"));
-
-
 
 // function getlength(e){
 //   console.log(e.split());
@@ -162,52 +144,44 @@ function myFun(e){
 //     // console.log(alpha);
 //     if(alpha==="L"|| alpha ==='l'){
 //       count +=  alpha.length
-      
+
 //     }
-    
+
 //   })
 //   return count
 //   console.log(count);
-  
 
 // }
 // console.log(getlength("HelLo world"));
 
-
-
 // Getting Data In Ascending Order using Sort Method
 
-
- function getNumberInAscendingOrder(data){
-let sorted = data
-sorted.sort((a,b)=>a -b)
-  return sorted
- }
+function getNumberInAscendingOrder(data) {
+  let sorted = data;
+  sorted.sort((a, b) => a - b);
+  return sorted;
+}
 //  console.log(getNumberInAscendingOrder([10,9,3,4,5,7]));
-
-
 
 //Descending Order
 
- function getNumberInDesendingOrder(data){
-  let descenting = data
-  return descenting.sort((a,b)=> b-a)
- }
+function getNumberInDesendingOrder(data) {
+  let descenting = data;
+  return descenting.sort((a, b) => b - a);
+}
 //  console.log(getNumberInDesendingOrder([12,3,4,5,6,8,9,0]));
- function myFunn(e){
-   let index1 = e[0]
-   let index2 = e[1]
-   if(index1.length < index2 ){
-    return index2
-   }
+function myFunn(e) {
+  let index1 = e[0];
+  let index2 = e[1];
+  if (index1.length < index2) {
+    return index2;
+  }
 
-   console.log([,,index2]);
+  console.log([, , index2]);
+}
+console.log(myFunn(["hello", "javscript"]));
 
- }
-console.log(myFunn(["hello","javscript"]));
-
-
-
-let arr = ["a","b","c"]
-let [,,indexOne] = arr
+let arr = ["a", "b", "c"];
+let [, , indexOne] = arr;
 console.log(indexOne);
+
