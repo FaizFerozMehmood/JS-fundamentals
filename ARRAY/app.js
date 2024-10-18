88c97648dab667a610eb88533301560e686d0dae
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-10-18 11:27:50 +0500

    object spread opertor in JS

--- ARRAY/app.js
+++ ARRAY/app.js
@@ -1,41 +1,54 @@
 //push() adds an element at the end of an array and returns new length
-const arr = [1,2,3,4,5]
-const vlu = arr.push("clock")
+const arr = [1, 2, 3, 4, 5];
+const vlu = arr.push("clock");
 console.log(vlu);
 
-
-
 //returning first element from an array
-const arry = [1,2,3,45,]
-const arry2 = arry[0]
+const arry = [1, 2, 3, 45];
+const arry2 = arry[0];
 console.log(arry2);
 
 // returning last element of the array using at()
 
-const ary = [1,2,3,44,5,6,7]
-const vl = ary.at(-1)
+const ary = [1, 2, 3, 44, 5, 6, 7];
+const vl = ary.at(-1);
 console.log(vl);
 
-
-
-function fun(sumVlues){
-    let res = 0
-    sumVlues.forEach((data) => {
-        if(data >0){
-          res += data
-        }
-    })
-    return res
-
+function fun(sumVlues) {
+  let res = 0;
+  sumVlues.forEach((data) => {
+    if (data > 0) {
+      res += data;
+    }
+  });
+  return res;
 }
-console.log("results=> "+fun([1,1,1,1,-5]));
+console.log("results=> " + fun([1, 1, 1, 1, -5]));
 
-
-
-function myfun(number,secNum){
-      return number.find((num)=>{
-        return num ===secNum
-      })
+function myfun(number, secNum) {
+  return number.find((num) => {
+    return num === secNum;
+  });
 }
 
-console.log(myfun([1,2,3,4,5],5));
+console.log(myfun([1, 2, 3, 4, 5], 5));
+
+const obj1 = {
+  Name: "faiz",
+  experience: "3 Years",
+};
+const obj2 = {
+  uname: "Habib ALi",
+  profession: " Senior Developer",
+};
+const obj3 = {
+  Username: "umair",
+  workedAs: "Trainer",
+};
+const result = {
+  created: "10/18/2024",
+  ...obj1,
+  ...obj2,
+  ...obj3,
+};
+console.log(result);
