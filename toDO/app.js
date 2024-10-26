3e8fe75dbe85d2e8fb61b620a32d2d57c6ad9bc9
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-10-26 10:15:08 +0500

    COMMIT

--- toDO/app.js
+++ toDO/app.js
@@ -5,7 +5,20 @@ const content = document.getElementById('content');
 
 add.addEventListener('click',()=>{
     console.log(input.value);
-    let display = `<li>${input.value}</li>`
-    content.innerHTML +=display
-    input.value =""
+    if(input.value.trim()){
+
+        localStorage.setItem("Inputvalue",input.value)
+        const storedValue = localStorage.getItem('Inputvalue')
+        console.log(storedValue);
+        
+        let display = `<li>${storedValue}</li>`
+        content.innerHTML +=display
+        input.value =""
+    }
+})
+window.addEventListener('DOMContentLoaded',()=>{
+    const savedValue = localStorage.getItem('Inputvalue')
+    if(savedValue){
+        input.value = savedValue
+    }
 })
