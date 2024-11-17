8819c4d4c7d857b80e7b9e62f715975fc12eacbc
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-17 14:20:01 +0500

    fizzBuzz kuch alg tarike se

--- New folder/app.js
+++ New folder/app.js
@@ -36,9 +36,9 @@
 
 // localStorage.setItem('userInfo',JSON.stringify(obj))
 
-// const storedObj = localStorage.getItem('userInfo')
-// if(storedObj){
-//     const userData = JSON.parse(storedObj)
+// const fizzObj = localStorage.getItem('userInfo')
+// if(fizzObj){
+//     const userData = JSON.parse(fizzObj)
 //     console.log(userData);
 
 // }else{
@@ -90,132 +90,158 @@ console.log(convertToNumber(20));
 console.log(convertToNumber(true));
 
 // Coding Question 5: Develop a JavaScript function named formatNumber(num, decimalPlaces)
-//  that formats a number num to a string 
+//  that formats a number num to a string
 //  representation with the specified number of decimal places decimalPlaces.
 //   Ensure that the function returns a string even if the input is not a valid number.
-// Hint: Use the .toFixed() method to control 
+// Hint: Use the .toFixed() method to control
 // the number of decimal places. Remember to
 //  handle non-numeric inputs gracefully
 //   to prevent errors.
-function formatNumber(num,decimalPlaces){
-    if(isNaN(num)){
-        return "NaN";
-
-    }
-    return Number(num).toFixed(decimalPlaces)
+function formatNumber(num, decimalPlaces) {
+  if (isNaN(num)) {
+    return "NaN";
+  }
+  return Number(num).toFixed(decimalPlaces);
 }
 
-console.log(formatNumber(3.14159, 2))
-console.log(formatNumber("3.14159, 2"))
-console.log(formatNumber("abc", 2))
-let nmbr = "0"
-let nmbr2 = "1"
-const btn = document.createElement('button')
-btn.innerText = "click here"
-document.body.appendChild(btn)
-btn.addEventListener('click',()=>{
-let respose = Number(nmbr) + Number(nmbr2)
-const p = document.createElement('p')
-nmbr = respose
-p.innerText = nmbr
-document.body.appendChild(p)
+console.log(formatNumber(3.14159, 2));
+console.log(formatNumber("3.14159, 2"));
+console.log(formatNumber("abc", 2));
+let nmbr = "0";
+let nmbr2 = "1";
+const btn = document.createElement("button");
+btn.innerText = "click here";
+document.body.appendChild(btn);
+btn.addEventListener("click", () => {
+  let respose = Number(nmbr) + Number(nmbr2);
+  const p = document.createElement("p");
+  nmbr = respose;
+  p.innerText = nmbr;
+  document.body.appendChild(p);
+});
 
-})
+let strvalue = "maan";
+let reverse = strvalue.split("").reverse().join("");
 
-let strvalue = "maan"
- let reverse = strvalue.split('').reverse().join('')
- 
 // let isPalindrome = true;
-if(strvalue === reverse){
+if (strvalue === reverse) {
   console.log(`${strvalue} is palindrome`);
-  
-}
-else{
+} else {
   console.log(`${strvalue} is not palindrome`);
-
 }
 //descending order
-let sortt  = [1,2,3,4,56,]
-let newone = sortt.sort((a,b)=>{
-  return b-a
-})
+let sortt = [1, 2, 3, 4, 56];
+let newone = sortt.sort((a, b) => {
+  return b - a;
+});
 console.log(newone);
 
 // ascending oder
-let ascending = [0,39,3,45,6,7,7]
+let ascending = [0, 39, 3, 45, 6, 7, 7];
 
-let getAscending = ascending.sort((a,b)=>{
-  return a-b
-})
+let getAscending = ascending.sort((a, b) => {
+  return a - b;
+});
 console.log(getAscending);
 
-
-let fruits = ["orange","apple"]
-fruits.sort()
-console.log(fruits);  // ['apple', 'orange']
+let fruits = ["orange", "apple"];
+fruits.sort();
+console.log(fruits); // ['apple', 'orange']
 
 // reverse//
-let rev = [0,39,3,45,6,7,7]
-rev.reverse()
+let rev = [0, 39, 3, 45, 6, 7, 7];
+rev.reverse();
 console.log(rev);
 
-
-let ind = ['faiz',"umaiar","ali"]
-let getValue = ind.indexOf("ali")
-if(getValue !==-1){
-  ind.splice(getValue,1)
+let ind = ["faiz", "umaiar", "ali"];
+let getValue = ind.indexOf("ali");
+if (getValue !== -1) {
+  ind.splice(getValue, 1);
 }
 console.log(ind);
 
-let indd = ['faiz',"umaiar","ali"]
-let getInd = indd.indexOf('faiz')
-if(getInd !==-1){
-  indd.splice(getInd,1)
+let indd = ["faiz", "umaiar", "ali"];
+let getInd = indd.indexOf("faiz");
+if (getInd !== -1) {
+  indd.splice(getInd, 1);
 }
 console.log(getInd);
 console.log(indd);
 
-let numAr = [0,2,3,4]
+let numAr = [0, 2, 3, 4];
 let count = 0;
-numAr.forEach((num)=>{
-  if(num){
-  count ++
+numAr.forEach((num) => {
+  if (num) {
+    count++;
   }
-})
+});
 console.log(count);
 
-
-let arry = [2,2,3,4,4,5,7,7]
-function removeDuplictate(abc){
-  let unique = []
-  for(let i = 0; i < abc.length;i++){
-    if(!unique.includes(abc[i]))
-     unique.push(abc[i])
+// remove removeDuplictate
+// let arry = [2,2,3,4,4,5,7,7]
+function removeDuplictate(abc) {
+  let unique = [];
+  for (let i = 0; i < abc.length; i++) {
+    if (!unique.includes(abc[i])) unique.push(abc[i]);
   }
 
-  return unique
+  return unique;
 }
-console.log(removeDuplictate([1,1,2,2,3,3]));
+console.log(removeDuplictate([1, 1, 2, 2, 3, 3]));
+console.log(removeDuplictate([1, 1, 3, 2, 3, 3]));
 
-
-function pali(vl){
-  let pre = vl.toLowerCase().replace(/[^a-z0-9]/g, '')
-let res = pre.split('').reverse().join('').toLowerCase()
-console.log(res,",,,,,", pre);
-return pre ===res
-
+function pali(vl) {
+  let pre = vl.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let res = pre.split("").reverse().join("").toLowerCase();
+  console.log(res, ",,,,,", pre);
+  return pre === res;
 }
-console.log(pali('A man, a plan, a canal, Panama'));
+console.log(pali("A man, a plan, a canal, Panama"));
 
-
-function countOccurance(arr,num){
-let leng;
-for(let i = 0; i<arr.length; i++){
-  if(arr[i]===num){
-leng++    
+function counteOccurence(arr, num) {
+  let leng = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      leng++;
+    }
   }
+  console.log("times of occurence of the 5 character  is =>", leng);
 }
+console.log(counteOccurence([1, 2, 2, 4, 5, 5, 5, 5], 5));
+
+// NESTED ARRAY INTO SINGLE-LEVEL ARRAY
+const nestedARRY=(arg)=>{
+console.log(arg);
+return arg.flat()
+
+}
+console.log(nestedARRY([1,2,3,4,[5,6],[7]]));
 
 
+// Write a function fizzBuzz that prints numbers from 1 to 100. For multiples of 3, print "Fizz";
+//  for multiples of 5, print "Buzz"; and for multiples
+//   of both 3 and 5, print "FizzBuzz".
+
+function printNumbers(vlue){
+  let fizz = 0
+// console.log(vlue,"vlue");
+if(vlue % 3 ==0){
+  // console.log("fizz",vlue);
+  return vlue
 }
-console.log(countOccurance([1,2,2,4,5,5,5,5,],5));
+if(vlue % 5 ==0){
+  console.log("buzz",vlue);
+}
+if(vlue % 3 ==0 && vlue % 5 ==0){
+  console.log("fizzbuzz",vlue);
+}
+
+}
+
+console.log(printNumbers());
+
+
+for(let i = 0; i<=100; i++){
+  let value = i
+  printNumbers(value)
+}
