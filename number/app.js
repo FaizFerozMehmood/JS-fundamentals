8198de24bff557f1da7ee40d135af78b3c29d938
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-19 17:46:02 +0500

    default value

--- number/app.js
+++ number/app.js
@@ -57,7 +57,18 @@ return count
 // console.log(getcount([1,2,3,3,3,3],3));
 let fun = getcount([1,2,3,3,3,3],3)
 console.log(fun);
+
+
+
 let numm = '1'
 let numPlus =   Number(numm) + Number("2")
 console.log(numPlus);
 
+function defaul(vlu="faiz"){
+if(!vlu){
+console.log("value mad");
+
+}
+return vlu
+}
+console.log(defaul());
