ab8253e0b58fd0dc6b5d6f38ee1594e0f502eaf5
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-25 17:23:48 +0500

    commit

---
+++ Random qts/app.js
@@ -0,0 +1,15 @@
+let quotes = [
+        { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+        { text: "Success is not the key to happiness. Happiness is the key to success.", author: "Albert Schweitzer" },
+        { text: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+        { text: "It does not matter how slowly you go as long as you do not stop.", author: "Confucius" },
+        { text: "Your time is limited, so don't waste it living someone else's life.", author: "Steve Jobs" },
+        { text: "Don't watch the clock; do what it does. Keep going.", author: "Sam Levenson" },
+        { text: "You miss 100% of the shots you don't take.", author: "Wayne Gretzky" },
+        { text: "The best way to predict the future is to invent it.", author: "Alan Kay" },
+        { text: "What you get by achieving your goals is not as important as what you become by achieving your goals.", author: "Zig Ziglar" },
+        { text: "In the middle of every difficulty lies opportunity.", author: "Albert Einstein" }
+      ];
+      
+  
+ 
