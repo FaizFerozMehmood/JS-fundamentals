98303a8e325c136642bd257929401313a78246dd
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-26 00:32:49 +0500

    commit

--- DOM/dm/myFile.js
+++ DOM/dm/myFile.js
@@ -1,34 +1,31 @@
-const ul = document.getElementById('ul')
-const btn = document.getElementById('btn')
-const input = document.getElementById('input')
+const ul = document.getElementById("ul");
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
 
-btn.addEventListener('click',myfun)
-function myfun(){
-console.log(input.value);
-// input.value= input.value
-// ul.innerHTML =`<li>${input.value}</li>`
-ul.innerHTML+=`
+btn.addEventListener("click", myfun);
+function myfun() {
+  console.log(input.value);
+  // input.value= input.value
+  // ul.innerHTML =`<li>${input.value}</li>`
+  ul.innerHTML += `
 <li>
 <span id ="span">${input.value} </span>
 <button onclick="editFun(this)">edit</button>
     <button onclick="delFun(this)">del</button>
 </li>
-`
-input.value =""
+`;
+  input.value = "";
 }
-function delFun(elememt){
-    console.log(elememt);
-    elememt.parentElement.remove()
- 
-    
+function delFun(elememt) {
+  console.log(elememt);
+  elememt.parentElement.remove();
 }
-function editFun(elememt){
-    console.log(elememt);
-   const pre =span.innerText
-   const updateValue = prompt("update todo ",pre)
-    console.log(updateValue);
-    pre.textContent=updateValue
-    
- 
-    
+function editFun(elememt) {
+  console.log(elememt);
+  const getEle = elememt.previousElementSibling;
+  let curentValue = getEle.innerText;
+  let updateValue = prompt("update todo ", curentValue);
+  if (updateValue !== null) {
+    getEle.innerText = curentValue;
+  }
 }
