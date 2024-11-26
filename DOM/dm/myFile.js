1370182d507b2b5d5b3108df4821be47ac6db800
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-26 11:58:34 +0500

    commit

--- DOM/dm/myFile.js
+++ DOM/dm/myFile.js
@@ -5,20 +5,32 @@ const input = document.getElementById("input");
 btn.addEventListener("click", myfun);
 function myfun() {
   console.log(input.value);
+  const existing = JSON.parse(localStorage.getItem("inputValue")) || []
+  existing.push(input.value)
   // input.value= input.value
   // ul.innerHTML =`<li>${input.value}</li>`
   ul.innerHTML += `
-<li>
-<span id ="span">${input.value} </span>
-<button onclick="editFun(this)">edit</button>
-    <button onclick="delFun(this)">del</button>
-</li>
-`;
+  <li>
+  <span id ="span">${input.value} </span>
+  <button onclick="editFun(this)">edit</button>
+  <button onclick="delFun(this)">del</button>
+  </li>
+  `;
+  localStorage.setItem("inputValue",JSON.stringify(existing))
   input.value = "";
+
 }
 function delFun(elememt) {
   console.log(elememt);
+  console.log(elememt.previousElementSibling.previousElementSibling.innerText);
+  let innerTXT = elememt.previousElementSibling.previousElementSibling.innerText
+  
   elememt.parentElement.remove();
+
+  // existing = existing.filter((item))
+  let existing = JSON.parse(localStorage.getItem("inputValue")) || []
+  existing = existing.filter((item)=> item!==innerTXT)
+  localStorage.setItem("inputValue",JSON.stringify(existing))
 }
 function editFun(elememt) {
   console.log(elememt);
@@ -28,4 +40,16 @@ function editFun(elememt) {
   if (updateValue !== null) {
     getEle.innerText = curentValue;
   }
+}
+window.onload =()=>{
+  let existing = JSON.parse(localStorage.getItem("inputValue")) || []
+existing.forEach(element => {
+  ul.innerHTML +=`
+   <li>
+        <span>${element}</span>
+        <button onclick="editFun(this)">edit</button>
+        <button onclick="delFun(this)">del</button>
+      </li>
+  `
+});
 }
