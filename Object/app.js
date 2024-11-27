ce4fa491061e02bbdb0dd78d142dabff5cd3de67
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-27 19:23:03 +0500

    commit

--- Object/app.js
+++ Object/app.js
@@ -1,10 +1,19 @@
 
 
-const obj = {
-  name:"faiz",
-  email:"faiz@gmail.com",
-  password:"12345"
-}
-obj[password] = 123452345
-console.log(obj[password]);
+// const obj = {
+//   name:"faiz",
+//   email:"faiz@gmail.com",
+//   password:"12345"
+// }
+const obj ={}
+obj.UserName = "faiz"
+obj.password = 23456
+obj.email = "faiz@gmail.com"
+console.log(obj);
 
+const newObj = new Object()
+newObj.name = "ali"
+newObj.password = 23456
+newObj.email = "faiz@gmail.com"
+newObj["address"] = "karachi"
+console.log(newObj["address"]);
