1be7156eade31026c94a8b0bd306b7928650b8ee
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-11-27 19:46:22 +0500

    this keyword in object

--- Object/app.js
+++ Object/app.js
@@ -16,4 +16,17 @@ newObj.name = "ali"
 newObj.password = 23456
 newObj.email = "faiz@gmail.com"
 newObj["address"] = "karachi"
+//You can access object properties in two way , dit notation and square barket
 console.log(newObj["address"]);
+
+const myobj = {
+  name:"faiz",
+  email:"faiz@gmail.com",
+  password:"12345",
+ greet : function(){
+console.log(`welcome ${this.name } your email is ${this.email}`)
+//this keyword refers to the curent execution context..
+
+ }
+}
+myobj.greet()
