54491f2a82f746a75dfb4c8789611999658e8e5a
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-12-04 21:27:10 +0500

    commit

--- asyncAwait/app.js
+++ asyncAwait/app.js
@@ -1,4 +1,19 @@
-fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+// fetch('https://fakestoreapi.com/products')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
+
+
+
+async function myfun(){
+try {
+  const response = await fetch('https://fakestoreapi.com/products')
+  const data = await response.json()
+  console.log("data",data);
+} catch (error) {
+  console.log(error);
+  
+}
+}
+myfun()
+
