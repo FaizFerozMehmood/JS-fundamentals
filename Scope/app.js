26689b6e5aa54c64e5b89e2ac9f4adad92ec158d
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-12-12 19:32:53 +0500

    commit

--- Scope/app.js
+++ Scope/app.js
@@ -1,6 +1,12 @@
-for(let i = 0; i<4;i++){
-    console.log(i);
-    setTimeout(() => {
-        console.log(i);
-    }, 0);
+// for(let i = 0; i<4;i++){
+//     console.log(i);
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 0);
+// }
+
+let value = "😁";
+if(1===1){
+    value = "😍"
 }
+console.log(value);
