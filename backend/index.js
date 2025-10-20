92671cdb3cd0e16011b7179eb5389b40d60ebfab
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2025-10-20 14:03:25 +0500

    implement post route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 2000;
 app.use(express.json());
+
 app.get("/", (req, res) => {
   try {
     res.status(200).json({ message: "HELLO API" });
@@ -10,15 +11,29 @@ app.get("/", (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-app.get("/:name",(req,res)=>{
-    const {name} = req.params
-    try {
-        res.status(200).json({message: `Helloo,${name}`})
-    } catch (error) {
-        res.status(500).json({message: error.message}) 
-    }
-})
+
+app.get("/:name", (req, res) => {
+  const { name } = req.params;
+  try {
+    res.status(200).json({ message: `Helloo,${name}` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/post", (req, res) => {
+  try {
+    const data = req.body;
+    console.log("recieived,", data);
+    res.status(201).json({ message: "data created ", item: data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
 });
 console.log("hello.....");
+
+// Add GET, POST, PUT, and DELETE routes for products.
