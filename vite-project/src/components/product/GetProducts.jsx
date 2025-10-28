3ee1fbad035412b07c843f29feb94e4a402d6b6a
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2025-10-28 18:51:55 +0500

    fetched products

--- vite-project/src/components/product/GetProducts.jsx
+++ vite-project/src/components/product/GetProducts.jsx
@@ -3,14 +3,40 @@ import React, { useEffect, useState } from "react";
 
 function GetProducts() {
   const [product, setProduct] = useState([]);
-  async function fetchData() {
-    const data = await axios.get("http://localhost:2000/product/get");
-    console.log("data", data);
-  }
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   useEffect(() => {
-    fetchData()
+    async function fetchData() {
+      try {
+        const data = await axios.get("http://localhost:2000/product/get");
+        if (data.status === 200) {
+          setProduct(data?.data?.data);
+        }
+      } catch (error) {
+        setError("error fetching product");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
   }, []);
-  return <div>GetProducts</div>;
+  console.log("product", product);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  return (
+    <div>
+      {product && product.length !== 0
+        ? product.map((data) => (
+            <div key={data._id}>
+              <h3>{data.title}</h3>
+              <p>{data.des}</p>
+              <p>{data.price}</p>
+              <img src={data.image} alt={data.title} width={100} />
+            </div>
+          ))
+        : "no products availabe!"}
+    </div>
+  );
 }
 
 export default GetProducts;
